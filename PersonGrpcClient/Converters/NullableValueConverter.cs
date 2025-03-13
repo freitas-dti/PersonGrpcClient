ad7014b993101250667b32368c13cab81d6381cb
Rodrigo Freitas <rodrigo.freitas@dtidigital.com.br>
2025-03-13 17:28:39 -0300

    feat: adjust number and float fields

---
+++ PersonGrpcClient/Converters/NullableValueConverter.cs
@@ -0,0 +1,50 @@
+﻿using System.Globalization;
+
+namespace PersonGrpcClient.Converters
+{
+    class NullableValueConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return string.Empty;
+
+            return value.ToString();
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return null;
+
+            string? strValue = value as string;
+
+            if (string.IsNullOrWhiteSpace(strValue))
+                return null;
+
+            try
+            {
+                if (targetType == typeof(int?))
+                {
+                    if (int.TryParse(strValue, out int result))
+                        return result;
+                    return null;
+                }
+
+                if (targetType == typeof(double?))
+                {
+                    if (double.TryParse(strValue, out double result))
+                        return result;
+                    return null;
+                }
+            }
+            catch
+            {
+                return null;
+            }
+
+            return null;
+        }
+    }
+}
+
